3dbd443ebbc8f0332ce9855cbc3c1424d58a6eff
netgio <gary@netgio.com>
2015-01-29 14:15:24 +0000

    removed superfluous folders

--- run.sh
+++ run.sh
@@ -1,8 +1,5 @@
-git add .
-git commit -m $1 
-docker build -t netgio/flint .
 APP=$(docker run -d -p 8080 netgio/flint)
 PORT=$(docker port $APP 8080| awk -F: '{print $2}')
 echo "Open http://localhost:$PORT/"
-curl http://localhost:$PORT
+curl http://localhost:$PORT/
 
